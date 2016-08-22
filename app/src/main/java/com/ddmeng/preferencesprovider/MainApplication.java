520d1c47dd063c92cdd8c51ae82fcd0a7b2ff2b6
Dandan MENG <mengdandanno1@163.com>
2016-08-22 18:38:48 +0800

    add stetho

---
+++ app/src/main/java/com/ddmeng/preferencesprovider/MainApplication.java
@@ -0,0 +1,13 @@
+package com.ddmeng.preferencesprovider;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Stetho.initializeWithDefaults(this);
+    }
+}
