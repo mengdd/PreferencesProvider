0c824e1a81be64adf9f2bb0c72e3d232379a99ce
Dandan MENG <mengdandanno1@163.com>
2016-08-23 12:09:12 +0800

    add PreferencesHelper to insert and query key-value

--- app/src/main/java/com/ddmeng/preferencesprovider/MainActivity.java
+++ app/src/main/java/com/ddmeng/preferencesprovider/MainActivity.java
@@ -1,13 +1,46 @@
 package com.ddmeng.preferencesprovider;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.ddmeng.preferencesprovider.provider.PreferencesHelper;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.key)
+    EditText keyInput;
+    @BindView(R.id.value)
+    EditText valueInput;
+    @BindView(R.id.value_output)
+    TextView valueOutput;
+
+    PreferencesHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        helper = new PreferencesHelper(this);
+    }
+
+    @OnClick(R.id.insert)
+    void insert() {
+        String key = keyInput.getText().toString();
+        String value = valueInput.getText().toString();
+        helper.insert("test module", key, value);
+    }
+
+    @OnClick(R.id.query)
+    void query() {
+        String key = keyInput.getText().toString();
+        String value = helper.query("test module", key);
+        valueOutput.setText(value);
     }
 }
---
+++ app/src/main/java/com/ddmeng/preferencesprovider/provider/PreferencesHelper.java
@@ -0,0 +1,54 @@
+package com.ddmeng.preferencesprovider.provider;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.ddmeng.preferencesprovider.provider.preferences.PreferencesContentValues;
+import com.ddmeng.preferencesprovider.provider.preferences.PreferencesCursor;
+import com.ddmeng.preferencesprovider.provider.preferences.PreferencesSelection;
+import com.ddmeng.preferencesprovider.utils.LogUtils;
+
+public class PreferencesHelper {
+
+    private static final String TAG = PreferencesHelper.class.getSimpleName();
+
+    private final Context context;
+    private final ContentResolver contentResolver;
+
+    public PreferencesHelper(Context context) {
+        this.context = context.getApplicationContext();
+        this.contentResolver = this.context.getContentResolver();
+
+    }
+
+    public void insert(@NonNull final String moduleName, @NonNull final String key, @Nullable String value) {
+        LogUtils.i(TAG, "insert: module " + moduleName + ", " + key + " = " + value);
+        PreferencesContentValues contentValues = new PreferencesContentValues();
+        contentValues.putModule(moduleName);
+        contentValues.putKey(key);
+        contentValues.putValue(value);
+        contentValues.insert(contentResolver);
+    }
+
+    public String query(@NonNull final String moduleName, @NonNull final String key) {
+        LogUtils.i(TAG, "query start: module " + moduleName + ", " + key);
+        String value = null;
+        PreferencesCursor cursor = new PreferencesSelection().module(moduleName).and().key(key).query(contentResolver);
+        if (cursor != null && cursor.moveToFirst()) {
+            value = cursor.getValue();
+        }
+        closeQuietly(cursor);
+        LogUtils.i(TAG, "query end: module " + moduleName + ", " + key + " = " + value);
+        return value;
+    }
+
+    private static void closeQuietly(Cursor cursor) {
+        if (cursor != null) {
+            cursor.close();
+        }
+    }
+
+}
